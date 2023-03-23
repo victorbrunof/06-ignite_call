61ee02106a5041ea03ed87643b00a94e45584c75
Victor Bruno <victorbrunof@icloud.com>
2023-03-22 22:25:35 -0300

    feat: Profile update - Persisting profile

--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -10,6 +10,7 @@ import {
   Text,
   TextInput,
 } from '@ignite-ui/react';
+import { useRouter } from 'next/router';
 import { ArrowRight } from 'phosphor-react';
 import { useForm, useFieldArray, Controller } from 'react-hook-form';
 import { z } from 'zod';
@@ -86,6 +87,8 @@ export default function TimeIntervals() {
     },
   });
 
+  const router = useRouter();
+
   const weekDays = getWeekDays();
 
   const { fields } = useFieldArray({
@@ -99,6 +102,8 @@ export default function TimeIntervals() {
     const { intervals } = data as TimeIntervalsFormOutput;
 
     await api.post('/users/time-intervals', { intervals });
+
+    await router.push('/register/update-profile');
   }
 
   return (
--- src/pages/register/update-profile/index.page.tsx
+++ src/pages/register/update-profile/index.page.tsx
@@ -1,4 +1,11 @@
-import { Button, Heading, MultiStep, Text, TextArea } from '@ignite-ui/react';
+import {
+  Avatar,
+  Button,
+  Heading,
+  MultiStep,
+  Text,
+  TextArea,
+} from '@ignite-ui/react';
 import { ArrowRight } from 'phosphor-react';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
@@ -9,6 +16,8 @@ import { useSession } from 'next-auth/react';
 import { GetServerSideProps } from 'next';
 import { getServerSession } from 'next-auth';
 import { buildNextAuthOptions } from '../../api/auth/[...nextauth].api';
+import { api } from '@/src/lib/axios';
+import { useRouter } from 'next/router';
 
 const updateProfileSchema = z.object({
   bio: z.string(),
@@ -26,10 +35,15 @@ export default function UpdateProfile() {
   });
 
   const session = useSession();
+  const router = useRouter();
 
-  console.log(session);
+  async function handleUpdateProfile(data: UpdateProfileData) {
+    await api.put('/users/profile', {
+      bio: data.bio,
+    });
 
-  async function handleUpdateProfile(data: UpdateProfileData) {}
+    await router.push(`/schedule/${session.data?.user.username}`);
+  }
 
   return (
     <Container>
@@ -46,6 +60,10 @@ export default function UpdateProfile() {
       <ProfileBox as="form" onSubmit={handleSubmit(handleUpdateProfile)}>
         <label>
           <Text size="sm">Foto de perfil</Text>
+          <Avatar
+            src={session.data?.user.avatar_url}
+            alt={session.data?.user.name}
+          />
         </label>
 
         <label>
